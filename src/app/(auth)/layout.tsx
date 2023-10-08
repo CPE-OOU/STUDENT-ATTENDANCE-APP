a09939e9e9089da68833ca609812b9862d2ad178
Ayomide <ayomidelawal700@gmail.com>
2023-10-08 20:10:15 +0100

    setup local fonts

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,21 @@
+import { getCurrentUser } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+
 interface AuthPageLayoutProps {
   children: React.ReactNode;
+  query: {};
 }
 
-const AuthPageLayout: React.FC<AuthPageLayoutProps> = ({ children }) => {
+const AuthPageLayout: React.FC<AuthPageLayoutProps> = async (context) => {
+  const { children } = context;
+  const user = await getCurrentUser();
+  console.log({ user });
+  if (user) {
+    if (user.emailVerified) {
+      return redirect('/dashboard');
+    }
+  }
+
   return <div className="container max-auto h-full">{children}</div>;
 };
 
