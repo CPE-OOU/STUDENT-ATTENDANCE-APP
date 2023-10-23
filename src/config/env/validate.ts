f59475bdcc6a096e1bcb22018dc17b9ec59d2420
Ayomide <ayomidelawal700@gmail.com>
2023-10-23 09:06:50 +0100

    fix env issue

--- src/config/env/validate.ts
+++ src/config/env/validate.ts
@@ -5,11 +5,11 @@ const EnvDefinedSchema = object({
   NOVU_API_KEY: string().min(1),
   NOVI_APP_HANDLER: string().min(1),
   NOVU_ENV_ID: string().min(1),
-  NEXTAUTH_URL: string().url(),
+  NEXTAUTH_URL: string().url().optional(),
   NEXT_SUPABASE_URL: string().url(),
   NEXT_SUPABASE_ANON: string(),
   NEXT_SUPABASE_SECRET_KEY: string(),
-  NEXT_PUBLIC_URL: string(),
+  NEXT_PUBLIC_URL: string().optional(),
 });
 
 const parsedEnv = EnvDefinedSchema.parse(process.env);
