c6c6aa9998be22bf42592282bd995126ea59801a
Ayomide <ayomidelawal700@gmail.com>
2023-10-24 17:49:59 +0100

    add verification guard

---
+++ src/app/(auth)/(routes)/verify-account/layout.tsx
@@ -0,0 +1,22 @@
+import { getCurrentUser } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+
+interface VerifyLayoutPage {
+  children: React.ReactNode;
+  query: {};
+}
+
+const VerifyLayoutPage: React.FC<VerifyLayoutPage> = async (context) => {
+  const { children } = context;
+  const user = await getCurrentUser();
+
+  if (!user) return redirect('/sign-in');
+
+  if (user.emailVerified) {
+    return redirect('/');
+  }
+
+  return <>{children}</>;
+};
+
+export default VerifyLayoutPage;
